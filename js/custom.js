dedf65b80f8c94b8714133c4f5ee8387db3102bc
stevemuir6 <68389769+stevemuir6@users.noreply.github.com>
2021-01-17 00:52:47 -0500

    updated

--- js/custom.js
+++ js/custom.js
@@ -13,17 +13,33 @@ function newFunction() {
     let userMonths = document.getElementById("term").value;
     let userIntRate = document.getElementById("intRate").value;
     
-    
+     
 
 //  calculate
  
     let principal = parseFloat(userAmount);
-    let calculateInterest = parseFloat(userIntRate) / 100 /12;
+    let calculateInterest = parseFloat(userIntRate * .01 ) /12;
     let calculatePayments = parseFloat(userMonths);
     
     
-//  
+//  monthly payment
     let x = Math.pow(1 + calculateInterest, calculatePayments)
-    return { userAmount, userMonths, userIntRate };
-    return { principal, calculateInterest, calculatePayments};
+    let monthly = (principal * x * calculateInterest) / (x-1);
+    let monthlyPayment = monthly.toFixed(2);
+
+    // Interest
+    let totalInterest = (monthly * calculatePayments - principal).toFixed(2);
+
+    // Total payment
+     let totalPayment = (monthly * calculatePayments).toFixed(2);
+
+
+    // results
+    document.getElementById("monthlyPayment").innerHTML = "$" + monthlyPayment;
+    document.getElementById("totalInt").innerHTML = "$" + totalInterest;
+    document.getElementById("totalPrinc").innerHTML = `$${userAmount}`;
+    document.getElementById("cost").innerHTML = totalPayment;
+    // document.getElementById("cost").innerHTML = "$" +
+    // return { userAmount, userMonths, userIntRate }
+    // return { principal, calculateInterest, calculatePayments};
 }
