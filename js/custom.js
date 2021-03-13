b3cb204a4ef08a62e4cfa47249bfbfdf5490e5f9
stevemuir6 <68389769+stevemuir6@users.noreply.github.com>
2021-03-13 16:36:45 -0500

    updated layout

--- js/custom.js
+++ js/custom.js
@@ -1,22 +1,3 @@
-(function () {
-    if (typeof self === 'undefined' || !self.Prism || !self.document || !document.querySelector) {
-        return;
-    }
-
-    Prism.plugins.toolbar.registerButton('download-file', function (env) {
-        var pre = env.element.parentNode;
-        if (!pre || !/pre/i.test(pre.nodeName) || !pre.hasAttribute('data-src') || !pre.hasAttribute('data-download-link')) {
-            return;
-        }
-        var src = pre.getAttribute('data-src');
-        var a = document.createElement('a');
-        a.textContent = pre.getAttribute('data-download-link-label') || 'Download';
-        a.setAttribute('download', '');
-        a.href = src;
-        return a;
-    });
-
-})();
 
 // listen for submit
 
@@ -107,7 +88,7 @@ function newFunction() {
     
 
     }
-
+    
     
 
     // ************************************************************
